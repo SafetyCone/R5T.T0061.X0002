dd6048580cbed06ff6e38988fd032f246000d3ab
davidcoats <david@getsafetycone.com>
2021-11-02 13:26:55 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0061.X0002.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0061.X0002.Construction
+{
+    /// <summary>
+    /// Construction project for Forensic IServiceCollection investigation..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0061.X0002/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0061.X0002
+{
+    /// <summary>
+    /// Forensic IServiceCollection investigation.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
